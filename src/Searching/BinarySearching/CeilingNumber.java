f07acaa41df7e3ce410b7edf0a4e471356066708
Sanskar Aggarwal <aggarwalsanskar992@gmail.com>
2022-04-21 00:43:28 +0530

    Ceil of a Number

---
+++ src/Searching/BinarySearching/CeilingNumber.java
@@ -0,0 +1,39 @@
+
+public class CeilingNumber {
+ public static void main(String[] args) {
+  int[] arr = { -12, 0, 3, 9, 13, 16, 25 };
+  int target = 15;
+  int ans = Ceiling(arr, target);
+  System.out.println(ans);
+ }
+
+ static int Ceiling(int[] arr, int target) {
+  int start = 0;
+  int end = arr.length - 1;
+  if (target >= arr[end]) {
+   return -1;
+  }
+  // find whether the array is in ascending or descending order
+  boolean isAsc = arr[end] > arr[start];
+  while (start <= end) {
+   int mid = start + (end - start) / 2;
+   if (arr[mid] == target) {
+    return mid;
+   }
+   if (isAsc) {
+    if (target > arr[mid]) {
+     start = mid + 1;
+    } else if (target < arr[mid]) {
+     end = mid - 1;
+    }
+   } else {
+    if (target < arr[mid]) {
+     start = mid + 1;
+    } else if (target > arr[mid]) {
+     end = mid - 1;
+    }
+   }
+  }
+  return arr[start];
+ }
+}
