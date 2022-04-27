41e897f03a4837afbec723579bf4bee00cee3a86
Sanskar Aggarwal <aggarwalsanskar992@gmail.com>
2022-04-27 21:29:17 +0530

    Bitwise Operator Basics

--- src/BitwiseOperator/setIthBit.java
+++ src/BitwiseOperator/setIthBit.java
@@ -1,5 +1,16 @@
 package BitwiseOperator;
 
+//set means setting the particular bit to 1 only not zero
 public class setIthBit {
- 
+ public static void main(String[] args) {
+  int n = 69;
+  int k = 5;
+  System.out.println(Integer.toBinaryString(setBit(n, k)));
+
+ }
+
+ static int setBit(int n, int k) {
+  return n | (1 << k - 1);
+ }
+
 }
