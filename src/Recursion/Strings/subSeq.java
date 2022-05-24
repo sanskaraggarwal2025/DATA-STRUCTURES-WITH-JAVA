b9fff3f5b75abe1ed371f73c6e1f9abdc7c089f7
Sanskar Aggarwal <aggarwalsanskar992@gmail.com>
2022-05-24 11:54:32 +0530

    Recursion in Strings

---
+++ src/Recursion/Strings/subSeq.java
@@ -0,0 +1,15 @@
+public class subSeq {
+ public static void main(String[] args) {
+  subSeq("", "abc");
+ }
+
+ static void subSeq(String p, String up) {
+  if (up.isEmpty()) {
+   System.out.println(p);
+   return;
+  }
+  char ch = up.charAt(0);
+  subSeq(p + ch, up.substring(1));
+  subSeq(p, up.substring(1));
+ }
+}
