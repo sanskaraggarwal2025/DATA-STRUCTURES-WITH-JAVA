8de6522a259a37b1203b6f9a2579e03bdedd929f
Sanskar Aggarwal <aggarwalsanskar992@gmail.com>
2022-05-27 15:01:40 +0530

    Circular Linked List

---
+++ src/LinkedList/CircularLinkedList/main.java
@@ -0,0 +1,11 @@
+
+public class main {
+ public static void main(String[] args) {
+  CLL list = new CLL();
+  list.insert(12);
+  list.insert(16);
+  list.insert(18);
+  list.insert(13);
+  list.display();
+ }
+}
