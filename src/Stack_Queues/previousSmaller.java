269e152d1bf45bb3a09d87ddfe928092e43d5ca7
Sanskar Aggarwal <aggarwalsanskar992@gmail.com>
2022-06-12 23:01:30 +0530

    Stack Problem

---
+++ src/Stack_Queues/previousSmaller.java
@@ -0,0 +1,26 @@
+
+
+import java.util.*;
+
+public class previousSmaller {
+ public static void main(String[] args) {
+
+  Stack<Integer> s = new Stack<>();
+  int[] a = { 4, 10, 5, 8, 20, 15, 3, 12 };
+
+  for (int i = 0; i < a.length; i++) {
+
+   while (!s.isEmpty() && s.peek() >= a[i]) {
+    s.pop();
+
+   }
+
+   if (s.isEmpty()) {
+    System.out.print(-1 + " ");
+   } else {
+    System.out.print(s.peek() + " ");
+   }
+   s.push(a[i]);
+  }
+ }
+}
